577b46abd5acc644aa03267f8344d02726f676e4
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-02-27 10:39:00 +0300

    Ohhhh it's was toooooo much

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+using TodoAppService;
+
+
+namespace GrpcClient;
+internal class Program
+{
+    private static async void Main(string[] args)
+    {
+        const string ServerAddress = "";
+
+        var channel = GrpcChannel.ForAddress(ServerAddress);
+
+        var client = new Greeter.GreeterClient(channel);
+
+		try
+		{
+            var newTodo = await client.CreateTodoAsync(new Todo { Discription = "Complete assigment" });
+            Console.WriteLine($"Created todo with ID: {newTodo.Id}");
+            var readTodo = await client.ReadTodoAsync(new TodoId { Id = newTodo.Id });
+            Console.WriteLine($"Read todo: id = {readTodo.Id} ,  Description = {readTodo.Discription}");
+
+
+		
+    }
+}
