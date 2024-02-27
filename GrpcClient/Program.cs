003385e73d4039b049975516113b2d60dbba1284
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-02-27 10:39:49 +0300

    HAH

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -13,13 +13,14 @@ internal class Program
 
         var client = new Greeter.GreeterClient(channel);
 
-		try
-		{
+        try
+        {
             var newTodo = await client.CreateTodoAsync(new Todo { Discription = "Complete assigment" });
             Console.WriteLine($"Created todo with ID: {newTodo.Id}");
             var readTodo = await client.ReadTodoAsync(new TodoId { Id = newTodo.Id });
             Console.WriteLine($"Read todo: id = {readTodo.Id} ,  Description = {readTodo.Discription}");
-
+        }
+        catch (Exception ex) { } //Dodelat !!!!
 
 		
     }
