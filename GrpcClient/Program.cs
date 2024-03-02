78ef3bb5f0a84b46c6513c386fc7bfd1a21d7284
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-03-02 18:19:42 +0300

    Program.cs/console/client

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using TodoAppService;
 
 
@@ -16,12 +17,31 @@ internal class Program
         try
         {
             var newTodo = await client.CreateTodoAsync(new Todo { Discription = "Complete assigment" });
+            
             Console.WriteLine($"Created todo with ID: {newTodo.Id}");
+            
             var readTodo = await client.ReadTodoAsync(new TodoId { Id = newTodo.Id });
+            
             Console.WriteLine($"Read todo: id = {readTodo.Id} ,  Description = {readTodo.Discription}");
-        }
-        catch (Exception ex) { } //Dodelat !!!!
 
-		
+            var updateTodo = await client.UpdateTodoAsync(newTodo);
+            await Console.Out.WriteLineAsync($"Updated Todo : ID ={updateTodo.Id} ");
+
+            await client.DeleteTodoAsync(new TodoId
+            {
+                Id = updateTodo.Id
+            });
+            await Console.Out.WriteLineAsync("Todo Deleted");
+        }
+        catch (RpcException ex) 
+        {
+            await Console.Out.WriteLineAsync(ex.Message);
+
+        }
+        await channel.ShutdownAsync();
+
+        await Console.Out.WriteLineAsync("Press any key to exit ");
+
+
     }
 }
